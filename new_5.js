bdeda58f988652ea903e767d8e9bf790869ab64f
somnambula <anty.lamerv@gmail.com>
2017-06-05 02:05:36 +0300

    v2

--- new_5.js
+++ new_5.js
@@ -1,22 +1,26 @@
 var minefield = [];
-var pole_x = 0,pole_y = 0,mine_sum = 0,mine_check = 0;
+var scale = 40; 
+var border_width = 4;
+var pole_x = 0,pole_y = 0,mine_sum = 0,mine_check = 0, lic = 0;
 	
 	function pole(n,m)
 	{
-		enter.hidden = true;
+		
 		var table = document.createElement("table");
 		table.setAttribute("id","game");
-		table.style.width = 44*m+"px";
-		table.style.height = 44*n+"px";
+		table.style.width = ((scale+border_width)*m+border_width)+"px";
+		table.style.height = ((scale+border_width)*n+border_width)+"px";
 		for(i = 0; i < n; i++)
 		{
 			var tr = document.createElement("tr");
 			for(j = 0; j < m; j++)
 			{
 				var td = document.createElement("td");
-				td.setAttribute("onclick",("try_lose("+i+","+j+"); check_win();"));
-				td.style.width= "40px";
-				td.style.height= "40px";
+				td.setAttribute("oncontextmenu","flag("+i+","+j+"); return false;");
+				td.setAttribute("onclick","try_lose("+i+","+j+"); check_win();");
+				td.style.width= scale+"px";
+				td.style.height= scale+"px";
+				td.style.border = border_width+"px solid cyan";
 				td.style.backgroundColor = "red";
 				tr.appendChild(td);
 			}
@@ -24,12 +28,16 @@ var pole_x = 0,pole_y = 0,mine_sum = 0,mine_check = 0;
 		}
 		document.body.appendChild(table);
 	}
-	function check_win()
+	
+		function check_win()
 	{
 		mine_check = 0;
 		for (i = 0; i < pole_x; i++)
 			for (j = 0; j < pole_y; j++)
-				if (game.children[i].children[j].style.backgroundColor == "red") mine_check++; 
+				if ((game.children[i].children[j].style.backgroundColor == "red")||(game.children[i].children[j].style.backgroundColor == "blue"))
+				{
+					mine_check++;
+				}
 			if (mine_sum == mine_check)
 			{
 				for (i = 0; i < pole_x; i++)
@@ -37,50 +45,74 @@ var pole_x = 0,pole_y = 0,mine_sum = 0,mine_check = 0;
 					for (j = 0; j < pole_y; j++)
 						{
 							if (minefield[i][j] == 1) game.children[i].children[j].style.backgroundColor = "black";
+							if ((minefield[i][j] != 1) && (game.children[i].children[j].style.backgroundColor == "blue")) game.children[i].children[j].style.backgroundColor = "green";
 							game.children[i].children[j].removeAttribute("onclick");
+							game.children[i].children[j].removeAttribute("oncontextmenu");
 						}
 				}
-				soundClick_win();
 				setInterval("next();", 7500);
+				soundClick_win();
+
 			}
 	}
 	
+	function flag (n,m)
+	{
+		if (game.children[n].children[m].style.backgroundColor == "blue") 
+		{
+			game.children[n].children[m].style.backgroundColor = "red";
+			game.children[n].children[m].setAttribute("onclick","try_lose("+n+","+m+"); check_win();");
+		} else
+		{
+		game.children[n].children[m].style.backgroundColor = "blue";
+		game.children[n].children[m].removeAttribute("onclick");
+		}
+	}
+	
+	
 	function try_lose(n,m)
 	{
-		if ((((n) >= 0)&&((m) >= 0))&&(((n) < pole_x)&&((m) < pole_y)))
-			if ((game.children[n].children[m].style.backgroundColor != "yellow")&&(game.children[n].children[m].style.backgroundColor != "white"))
-				if (minefield[n][m] == 1) 
-				{
-					soundClick_lose();
-					next();
-				}else
-				{
-					var sum = 0;
-					for (i = -1; i <= 1; i++)
+			if ((((n) >= 0)&&((m) >= 0))&&(((n) < pole_x)&&((m) < pole_y)))
+				if ((game.children[n].children[m].style.backgroundColor != "yellow")&&(game.children[n].children[m].style.backgroundColor != "white")&&(game.children[n].children[m].style.backgroundColor != "blue"))
+					if (minefield[n][m] == 1) 
 					{
-						for (j = -1; j <= 1; j++)
-						{	
-							if ((((n+i) >= 0)&&((m+j) >= 0))&&(((n+i) < pole_x)&&((m+j) < pole_y))) {sum += minefield[n+i][m+j];}
-							if ((i == 0)&&(j == 0)){sum -= minefield[n+i][m+i];}
+						soundClick_lose();
+						next();
+					}else
+					{
+						var sum = 0;
+						for (i = -1; i <= 1; i++)
+						{
+							for (j = -1; j <= 1; j++)
+							{	
+								if ((((n+i) >= 0)&&((m+j) >= 0))&&(((n+i) < pole_x)&&((m+j) < pole_y))) {sum += minefield[n+i][m+j];}
+								if ((i == 0)&&(j == 0)){sum -= minefield[n+i][m+i];}
+							}
 						}
-					}
-					if (sum > 0) 
-					{
-						game.children[n].children[m].style.backgroundColor = "yellow";
-						game.children[n].children[m].innerHTML = sum;
-					} else
-					{
-						game.children[n].children[m].style.backgroundColor = "white";
-						try_lose(n-1,m-1);
-						try_lose(n-1,m);
-						try_lose(n-1,m+1);
-						try_lose(n,m-1);
-						try_lose(n,m+1);
-						try_lose(n+1,m-1);
-						try_lose(n+1,m);
-						try_lose(n+1,m+1);
-					}
-				}
+						if (sum > 0) 
+						{
+							game.children[n].children[m].style.backgroundColor = "yellow";
+							game.children[n].children[m].removeAttribute("onclick");
+							game.children[n].children[m].removeAttribute("oncontextmenu");
+							game.children[n].children[m].innerHTML = sum;
+						} else
+						{
+							game.children[n].children[m].style.backgroundColor = "white";
+							game.children[n].children[m].removeAttribute("onclick");
+							game.children[n].children[m].removeAttribute("oncontextmenu");
+							lic++;
+							try_lose(n-1,m-1);
+							try_lose(n-1,m);
+							try_lose(n-1,m+1);
+							try_lose(n,m-1);
+							try_lose(n,m+1);
+							try_lose(n+1,m-1);
+							try_lose(n+1,m);
+							try_lose(n+1,m+1);
+							
+						}
+					}	
+				
 	}
 	
 	function generate_minefield(n, m, k)
@@ -110,11 +142,14 @@ var pole_x = 0,pole_y = 0,mine_sum = 0,mine_check = 0;
 		}
 	}
 	
+	
 	function playgame()
 	{
 		pole_x = pole_xe.value;
 		pole_y = pole_ye.value;
 		mine_sum = mine_sume.value;
+		enter.hidden = true;
+		lic = 0;
 		generate_minefield(pole_x,pole_y,mine_sum);
 		pole(pole_x,pole_y);
 	}
@@ -147,7 +182,9 @@ var pole_x = 0,pole_y = 0,mine_sum = 0,mine_check = 0;
 			for (j = 0; j < pole_y; j++)
 				{
 					if (minefield[i][j] == 1) game.children[i].children[j].style.backgroundColor = "black";
+					if ((minefield[i][j] != 1) && (game.children[i].children[j].style.backgroundColor == "blue")) game.children[i].children[j].style.backgroundColor = "green";
 					game.children[i].children[j].removeAttribute("onclick");
+					game.children[i].children[j].removeAttribute("oncontextmenu");
 				}
 		}
 		var img = document.createElement("img");
@@ -165,12 +202,12 @@ var pole_x = 0,pole_y = 0,mine_sum = 0,mine_check = 0;
 	
 	function check_polex()
 	{
-		if ((isNaN(pole_xe.value))||(pole_xe.value < 0)||(pole_xe.value > 500)||((pole_xe.value % 1) != 0)) pole_xe.value = "";
+		if ((isNaN(pole_xe.value))||(pole_xe.value < 0)||(pole_xe.value > 100)||((pole_xe.value % 1) != 0)) pole_xe.value = "";
 	}
 	
 	function check_poley()
 	{
-		if ((isNaN(pole_ye.value))||(pole_ye.value < 0)||(pole_ye.value > 500)) pole_ye.value = "";
+		if ((isNaN(pole_ye.value))||(pole_ye.value < 0)||(pole_ye.value > 100)||((pole_ye.value % 1) != 0)) pole_ye.value = "";
 	}
 	
 	function check_x_y()
